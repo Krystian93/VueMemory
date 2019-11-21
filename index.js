cecb9e745383793082837c1aa56134f4293ae2ff
Krystian <w.krystian93@gmail.com>
2019-11-21 20:25:23 +0100

    basic working version

--- index.js
+++ index.js
@@ -1,74 +1,69 @@
 Vue.component("card", {
   props: {
-    id:Number,
-    color: String,
-    show: Boolean
+    card: Object
   },
 
-
-  methods:{
-    clickCard(){
-      this.$emit('show-card',this);
-    },
+  methods: {
+    clickCard() {
+      if (!this.card.show) {
+        this.$emit("click-card", this.card);
+      }
+    }
   },
-
 
   template: ` 
-    <div @click='clickCard'  class="card" :style="{backgroundColor: this.show ? this.color : 'silver'}">
+    <div @click="clickCard"  class="card" :style="{backgroundColor: card.show ? card.color : 'silver'}">
     </div>
   `
 });
 
 const app = new Vue({
+  name: "board",
   el: "#board",
 
-  name:'board',
-
   data: {
+    clickedCards: [],
+    showingCards: false,
+
     cards: [
-      { id:1,
-        color: "#9E2B25",
-        show: false
-      },
-      { id:2,
-        color: "#9E2B25",
-        show: false
-      },
-      { id:3,
-        color: "#51355A",
-        show: false
-      },
-      { id:4,
-        color: "#51355A",
-        show: false
-      },
-      { id:5,
-        color: "#43AA8B",
-        show: false
-      },
-      { id:6,
-        color: "#43AA8B",
-        show: false
-      },
-      { id:7,
-        color: "#B2B09B",
-        show: false
-      },
-      { id:8,
-        color: "#B2B09B",
-        show: false
-      }
+      { id: 1, color: "#9E2B25", show: false },
+      { id: 2, color: "#9E2B25", show: false },
+      { id: 3, color: "#51355A", show: false },
+      { id: 4, color: "#51355A", show: false },
+      { id: 5, color: "#43AA8B", show: false },
+      { id: 6, color: "#43AA8B", show: false },
+      { id: 7, color: "#B2B09B", show: false },
+      { id: 8, color: "#B2B09B", show: false }
     ]
   },
 
   methods: {
-    showCard(that) {
-       this.cards.find(card=>{
-         if(card.id===that.id){
-           card.show=true;
-         }
-       });
-       
+    clickCard(card) {
+      console.log("click");
+      if (this.showingCards === false) {
+        if (this.clickedCards.length != 2) {
+          card.show = true;
+          this.clickedCards.push(card);
+        }
+        if (this.clickedCards.length == 2) {
+          this.checkCards();
+        }
+      }
     },
-  },
+
+    checkCards() {
+      if (this.clickedCards[0].color === this.clickedCards[1].color) {
+        console.log("match");
+        this.clickedCards = [];
+      } else {
+        this.showingCards = true;
+        setTimeout(() => {
+          this.clickedCards[0].show = false;
+          this.clickedCards[1].show = false;
+          this.clickedCards = [];
+          this.showingCards = false;
+        }, 1000);
+      }
+    }
+  }
 });
