d9fa0ad704f71c6468f4eca6fa1bdfcb2a6d1ab4
Krystian <w.krystian93@gmail.com>
2019-11-23 09:27:31 +0100

    add fuffle function and animation of cards

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ Vue.component("card", {
     }
   },
 
+
   template: ` 
     <div @click="clickCard"  class="card" :style="{backgroundColor: card.show ? card.color : 'silver'}">
     </div>
@@ -37,9 +38,16 @@ const app = new Vue({
     ]
   },
 
+  created() {
+    this.shuffle(this.cards);
+  },
+  updated() {
+   console.log('parent forced to updete');
+  },
+
+
   methods: {
     clickCard(card) {
-      console.log("click");
       if (this.showingCards === false) {
         if (this.clickedCards.length != 2) {
           card.show = true;
@@ -53,7 +61,6 @@ const app = new Vue({
 
     checkCards() {
       if (this.clickedCards[0].color === this.clickedCards[1].color) {
-        console.log("match");
         this.clickedCards = [];
       } else {
         this.showingCards = true;
@@ -64,6 +71,31 @@ const app = new Vue({
           this.showingCards = false;
         }, 1000);
       }
-    }
+    },
+
+
+//The modern version of the Fisher–Yates shuffle
+    shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+      }
+      return a;
+    },
+
+
+    restartGame(){
+      if(!this.showingCards){
+        this.cards.forEach(card=>card.show = false);
+        this.clickedCards = [];
+        this.showingCards = false;
+        this.cards = this.shuffle(this.cards);
+          this.$forceUpdate(); // little hack - parent component is not updating when items in cards array are chaning places..
+      }
+    },
+
   }
 });
